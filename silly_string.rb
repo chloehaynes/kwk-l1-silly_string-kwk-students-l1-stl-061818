66c251f9b5c06b94c42bf17a7ea96e59d6ddc4be
chloehaynes <19chaynes@nerinxhs.org>
2018-06-20 16:53:48 +0000

    automatically backed up by learn

---
+++ silly_string.rb
@@ -0,0 +1,32 @@
+favorite_word = "guitar"
+
+puts favorite_word
+
+
+puts favorite_word.upcase
+
+
+phrase = "IM NOT SHOUTING"
+
+puts phrase.downcase
+
+
+string = "supercaligragilisticexpialodocious"
+
+puts string.length
+
+
+puts string + "wow" + "mom"
+
+
+sentence = "i really like programming"
+
+puts sentence.capitalize
+
+
+"Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
+
+
+switch = "Astounding aardvarks, arguably an ancient animal always ate apples and acorns and artichokes all around Athens, amazing!"
+
+puts switch.gsub(/A/, '0').gsub(/a/, 'o')
